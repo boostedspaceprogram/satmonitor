fb6ff1940a1c77888a9a42321411018ed015b396
Umit Canbolat <umit@umitcanbolat.nl>
2023-03-17 01:44:51 +0100

    2D map

--- src/GUI/MDI.py
+++ src/GUI/MDI.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QMdiArea, QMdiSubWindow, QTextEdit, QStatusBar
-from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QMdiArea, QMdiSubWindow, QTextEdit
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+import folium
+
 
 class MDI():
     
@@ -25,8 +27,20 @@ class MDI():
         self.mdiArea.addSubWindow(sub)
         sub.show()
         
-        # add mdi sub window
-        sub2 = QMdiSubWindow()
-        sub2.setWidget(QTextEdit())
-        self.mdiArea.addSubWindow(sub2)
-        sub2.show()
+        
+        # Map sub window
+        sub_window = QMdiSubWindow()
+        folium_map = folium.Map(
+            location=[45.372, -121.6972],
+            zoom_start=1,
+            tiles='http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}&s=Ga',
+            attr='Google Imagery',
+        )
+        web_view = QWebEngineView()
+        
+        # web view no borders or scroll bars
+        web_view.setContentsMargins(0, 0, 0, 0)
+        web_view.setHtml(folium_map._repr_html_())
+        sub_window.setWidget(web_view)
+        self.mdiArea.addSubWindow(sub_window)
+        sub_window.show()
