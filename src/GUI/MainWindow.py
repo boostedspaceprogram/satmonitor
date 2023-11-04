3e9ef5d51dc41ce28e59da4ff5032f9b4a323316
Umit Canbolat <umit@umitcanbolat.nl>
2023-11-05 00:54:09 +0100

    changed default window size

--- src/GUI/MainWindow.py
+++ src/GUI/MainWindow.py
@@ -15,7 +15,7 @@ class MainWindow(QMainWindow):
     
     def __init__(self):
         QMainWindow.__init__(self, None)
-        self.resize(1400, 900)
+        self.resize(1280, 720)
         self.setWindowTitle("Home - Sat Monitor")
         self.setWindowIcon(get_icon("logo_dark"))
         self.setDockNestingEnabled(True)
